5f26dd7d2898a4bb42d0ab59066b91c1b3fe0643
webuiexpert <webuiexpert929@gmail.com>
2024-07-17 13:53:28 +0530

    first Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation:{
+        "loop-scroll":"loop-scroll 50s linear infinite",
+        },
+        keyframes:{
+          "loop-scroll":{
+            "from":{transform: "translateX(0)"},
+            "to":{transform: "translateX(-100%)"},
+            },
+      },
+    },
   },
   plugins: [],
 }
